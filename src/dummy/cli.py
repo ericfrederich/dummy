6b3be52969dc6b3c3bf1b37f5052c9ebc9e9a0b2
Eric L Frederich <eric.frederich@siemens.com>
2022-11-01 10:41:01 -0400

    Add code

---
+++ src/dummy/cli.py
@@ -0,0 +1,29 @@
+from pprint import pformat
+
+from pygments import highlight
+from pygments.formatters.terminal256 import Terminal256Formatter
+from pygments.lexers.data import JsonLexer, YamlLexer
+from pygments.lexers.python import Python3Lexer
+
+
+def pcformat(obj, lang="python", style="material"):
+    """Pretty format with color"""
+
+    if lang in ("python", "py"):
+        code = pformat(obj, indent=2)
+        lexer_cls = Python3Lexer
+    elif lang == "json":
+        code = json.dumps(obj, indent=2, sort_keys=True)
+        lexer_cls = JsonLexer
+    elif lang in ("yaml", "yml"):
+        code = yaml.safe_dump(obj)
+        lexer_cls = YamlLexer
+    else:
+        raise ValueError(format)
+    return highlight(code, lexer_cls(ensurenl=False), Terminal256Formatter(style=style))
+
+
+def cli_main():
+    print("HELLO!!!")
+    print(pcformat([1, 2, "three", 4]))
+    print("Bye!")
